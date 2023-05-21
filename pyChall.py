3ffd1f137f89b456b430805b1f94b00b3c44e7c3
bridge2nowhere <38335227+bridge2nowhere@users.noreply.github.com>
2023-05-20 18:20:32 -0700

    Started Pulling Match/Participant Data

--- pyChall.py
+++ pyChall.py
@@ -5,7 +5,7 @@ import config
 challonge.set_credentials(config.api_usr, config.api_key)
 
 # Retrieve a tournament by its id (or its url).
-tournament = challonge.tournaments.show("hg8z109a")
+tournament = challonge.tournaments.show("sn1rdt6a")
 
 # Tournaments, matches, and participants are all represented as normal Python dicts.
 print(tournament["id"]) # 3272
@@ -14,7 +14,14 @@ print(tournament["started_at"]) # None
 
 # Retrieve the participants for a given tournament.
 matches = challonge.matches.index(tournament["id"])
-print(matches)
+#for m in matches:
+
+#print(len(matches))
+
+participants = challonge.participants.index(tournament["id"])
+for i in participants:
+    print(i["name"],i["id"])
+
 #print(len(participants)) # 13
 
 # Start the tournament and retrieve the updated information to see the effects
